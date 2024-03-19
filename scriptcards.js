11b0ecea24010c4e1fd2792e5dbab6300433721d
Adriano Neves Ribeiro <adriano.neves@lojasrede.com.br>
2024-03-19 16:22:30 -0300

    paginas criadas

---
+++ scriptcards.js
@@ -0,0 +1,200 @@
+window.onload = function() { 
+    renderTags(characters); 
+    renderProject(characters);
+    renderEnvironment(environment);
+    renderDev(characters);
+    renderFrameworks(characters);
+    renderBugs(characters);
+
+    // image.addEventListener('click',(evt)=>{
+    //     desc.classList.toggle('hidden');
+    //     grid.classList.toggle('backgroundDark');
+    //     console.log('cliquei nos outros');
+    // })
+
+};
+
+
+const characters = [
+    'ASSEMBELE',
+    'C.MAISMAIS',
+    'CEH',
+    'CESHARP',
+    'CESSESSE',
+    'DARTY',
+    'ESKELLE',
+    'F.ORTRAM',
+    'GOH',
+    'HAGATEMELER',
+    'JAVAH',
+    'JAVASCRIPTO',
+    'KOTLEEN',
+    'PAITON',
+    'PEAGAPE',
+    'PERULU',
+    'RIUBI',
+    'RUUSSTY',
+    'SWIIFT',
+    'TAI PIS CRIP',
+    'VISUBAZICK',
+];
+
+const environment = [
+    'VESSE-ESTUDIO'
+]
+
+function renderTags(tagList){
+    const grid = document.querySelector('.grid');
+    grid.innerHTML = '';
+
+    tagList.forEach(tag => {
+
+        const tagElement = document.createElement('div');
+        const tagLabel = document.createElement('label');
+        const tagImage = document.createElement('img');
+        
+        tagElement.className = 'img-card';
+
+        tagImage.src = `../img/${tag}.png`;
+        tagImage.className = 'img-cardTeste';
+
+        tagLabel.textContent = tag;
+        tagLabel.className = 'nameCharacter';
+    
+        grid.appendChild(tagElement);
+        tagElement.appendChild(tagImage);
+        tagElement.appendChild(tagLabel);    
+    });
+}
+
+function renderProject(tagList){
+    const grid2 = document.querySelector('.grid2');
+    grid2.innerHTML = '';
+
+    tagList.forEach(tag => {
+        const tagElement = document.createElement('div');
+        const tagLabel = document.createElement('label');
+        const tagImage = document.createElement('img');
+        
+        tagElement.className = 'img-card';
+
+        tagImage.src = `../img/${tag}.png`;
+        tagImage.className = 'img-cardTeste';
+
+        tagLabel.textContent = tag;
+        tagLabel.className = 'nameCharacter';
+    
+        grid2.appendChild(tagElement);
+        tagElement.appendChild(tagImage);
+        tagElement.appendChild(tagLabel);    
+    })
+}
+
+function renderEnvironment(tagList){
+    const grid3 = document.querySelector('.grid3');
+    grid3.innerHTML = '';
+
+    tagList.forEach(tag => {
+        const tagElement = document.createElement('div');
+        const tagLabel = document.createElement('label');
+        const tagImage = document.createElement('img');
+        
+        tagElement.className = 'img-card';
+
+        tagImage.src = `../img/${tag}.png`;
+        tagImage.className = 'img-cardTeste';
+
+        tagLabel.textContent = tag;
+        tagLabel.className = 'nameCharacter';
+    
+        grid3.appendChild(tagElement);
+        tagElement.appendChild(tagImage);
+        tagElement.appendChild(tagLabel);    
+    })
+}
+
+function renderDev(tagList){
+    const grid4 = document.querySelector('.grid4');
+    grid4.innerHTML = '';
+
+    tagList.forEach(tag => {
+        const tagElement = document.createElement('div');
+        const tagLabel = document.createElement('label');
+        const tagImage = document.createElement('img');
+        
+        tagElement.className = 'img-card';
+
+        tagImage.src = `../img/${tag}.png`;
+        tagImage.className = 'img-cardTeste';
+
+        tagLabel.textContent = tag;
+        tagLabel.className = 'nameCharacter';
+    
+        grid4.appendChild(tagElement);
+        tagElement.appendChild(tagImage);
+        tagElement.appendChild(tagLabel);    
+    })
+}
+
+function renderFrameworks(tagList){
+    const grid5 = document.querySelector('.grid5');
+    grid5.innerHTML = '';
+
+    tagList.forEach(tag => {
+        const tagElement = document.createElement('div');
+        const tagLabel = document.createElement('label');
+        const tagImage = document.createElement('img');
+        
+        tagElement.className = 'img-card';
+
+        tagImage.src = `../img/${tag}.png`;
+        tagImage.className = 'img-cardTeste';
+
+        tagLabel.textContent = tag;
+        tagLabel.className = 'nameCharacter';
+    
+        grid5.appendChild(tagElement);
+        tagElement.appendChild(tagImage);
+        tagElement.appendChild(tagLabel);    
+    })
+}
+
+function renderBugs(tagList){
+    const grid6 = document.querySelector('.grid6');
+    grid6.innerHTML = '';
+
+    tagList.forEach(tag => {
+        const tagElement = document.createElement('div');
+        const tagLabel = document.createElement('label');
+        const tagImage = document.createElement('img');
+        
+        tagElement.className = 'img-card';
+
+        tagImage.src = `../img/${tag}.png`;
+        tagImage.className = 'img-cardTeste';
+
+        tagLabel.textContent = tag;
+        tagLabel.className = 'nameCharacter';
+    
+        grid6.appendChild(tagElement);
+        tagElement.appendChild(tagImage);
+        tagElement.appendChild(tagLabel);    
+    })
+}
+
+
+const linguagens = [
+    {
+        "nome": "Assembele",
+        "descricao": "Nascido nas profundezas das montanhas antigas, foi criado pelos espíritos da terra. Tem uma afinidade única com as pedras e os minerais. Sua arma é a Clava de Obsidiana(uma arma poderosa forjada por suas próprias mãos).",
+        "frameworks": "../TCC/img/VESSE-ESTUDIO.png",
+        "fusoes": "../TCC/img/CEH.png"
+    }
+]
+
+
+
+
+
+
+
---
+++ scriptenumcards.js
@@ -0,0 +1,13 @@
+
+
+const card = document.querySelector('.grid');
+
+card.addEventListener('click',(evt)=>{
+    //cardInformation.classList.remove('hidden');
+    console.log('cliquei');
+})
+
+if(cardChose == 'ASSEMBELE')
+{
+    cardName.innerHTML = linguagens.nome;
+}
